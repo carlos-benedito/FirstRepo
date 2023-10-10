e5bcd663548c7fdcbfcf099e0d96f14bc736b44b
user.email <carlos.benedito1012000@gmail.com>
2023-10-10 09:17:49 +0200

    hola mundo

---
+++ Main.java
@@ -0,0 +1,13 @@
+public static void Main (String[] args){
+	String salutattion = "hola mundo";
+	System.out.println(salutation);
+	String farewell = "adios con el corazon";
+	System.out.println(frewell);
+
+	int num1 = 4;
+	int num2 = 5;
+	System.out.println(addNumbbers(num1, num2));
+}
+public static Integer addNumbers(int a, int b){
+	return (a+b);
+}
